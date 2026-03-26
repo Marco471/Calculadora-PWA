a4f74f91b0994a55a5ac4c06e3e5c054166c8eb6
SeuNomeDeUsuario <seuemail@exemplo.com>
2026-03-26 10:28:47 -0300

    Primeiro commit da Calculadora-PWA

---
+++ script.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+
+  const display = document.getElementById("display");
+
+  function add(valor) {
+    display.value += valor;
+  }
+
+  function limpar() {
+    display.value = "";
+  }
+
+  function backspace() {
+    display.value = display.value.slice(0, -1);
+  }
+
+  function calcular() {
+    try {
+      display.value = eval(display.value);
+    } catch {
+      display.value = "Erro";
+    }
+  }
+
+  function porcentagem() {
+    try {
+      display.value = Number(display.value) / 100;
+    } catch {
+      display.value = "Erro";
+    }
+  }
+
+  window.add = add;
+  window.limpar = limpar;
+  window.backspace = backspace;
+  window.calcular = calcular;
+  window.porcentagem = porcentagem;
+};
